d08851ad7cad04037b930911dc761fad336f8430
stryapkov <igor.tryapkov@yandex.ru>
2019-02-04 22:07:34 +0300

    _

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -9,8 +9,6 @@ namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-
-
         public IActionResult Index() => View();
         public IActionResult Shop() => View();
         public IActionResult ProductDetails() => View();
